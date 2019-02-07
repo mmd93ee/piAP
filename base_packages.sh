cdb50f9b83229ad072f3ae75035cd5a8ac8a17e2
Matthew Dutton <dutton.matt@gmail.com>
2019-02-07 21:58:56 +0000

    base load with conditions

--- base_packages.sh
+++ base_packages.sh
@@ -3,9 +3,13 @@
 # Script to install base packages and updates to vanilla Pi deployment
 # Assumption is git is already installed :-)
 cd ~
-if [ %1 = os_update ]
+
+if [ -e os_updated ]
 then
+  echo "Initial OS Update Completed"
+else
   sudo apt-get update; sudo apt-get upgrade
+  touch os_updated
 fi
 
 if [ -e witty_installed ]
@@ -14,7 +18,6 @@ then
 else
   wget http://www.uugear.com/repo/WittyPi2/installWittyPi.sh
   sudo ./installWittyPi.sh
-  cd ~
   touch witty_installed
 fi
 
