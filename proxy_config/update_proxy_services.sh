66f34faad764cc70f3b25dce492b65cf7cf9ac5b
Matthew Dutton <dutton.matt@gmail.com>
2019-02-08 22:21:43 +0000

    Created squid config

--- proxy_config/update_proxy_services.sh
+++ proxy_config/update_proxy_services.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+# Proxy .pac file
+sudo cp -f ./piproxy/proxy_config/proxy.pac /usr/share/nginx/html/proxy.pac
+sudo chmod 0755 /usr/share/nginx/html/proxy.pac
+
 # Squid Proxy Config File
+sudo cp -f ./piproxy/proxy_config/squid.conf /etc/squid/squid.conf
 
 # nginx Config File
 
