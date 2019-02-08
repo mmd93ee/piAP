645975966e0114da0782b4e7d719cdfda3a0c54a
Matthew Dutton <dutton.matt@gmail.com>
2019-02-08 10:11:31 +0000

    update squidguard build

--- update_proxy_blacklist.sh
+++ update_proxy_blacklist.sh
@@ -5,7 +5,13 @@ cd ~
 # Check for old and delete if exist
 if [ -e /var/lib/squidguard/db/blacklists ]
 then
-  rm -rf /var/lib/squidguard/db/blacklists
+  sudo rm -rf /var/lib/squidguard/db/blacklists
+fi
+
+# If we crashed out then the new download has a different name, check if we already have the download and remove it
+if [ -e blacklists.tar.gz ]
+then
+  rm -f blacklists.tar.gz
 fi
 
 # Download new blacklists
@@ -15,5 +21,5 @@ rm blacklists.tar.gz
 sudo mv -fv ./blacklists /var/lib/squidguard/db/
 
 # Apply new files
-sudo squidGuard -C all
+sudo squidGuard -d -b -t -C all
 sudo chown -R proxy:proxy /var/lib/squidguard/db/
