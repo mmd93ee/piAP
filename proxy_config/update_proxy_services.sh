086f299b84fca37fd1f81329c329bcf14569385b
Matthew Dutton <dutton.matt@gmail.com>
2019-02-08 22:35:28 +0000

    Created squidguard config

--- proxy_config/update_proxy_services.sh
+++ proxy_config/update_proxy_services.sh
@@ -4,8 +4,14 @@
 sudo cp -f ./piproxy/proxy_config/proxy.pac /usr/share/nginx/html/proxy.pac
 sudo chmod 0755 /usr/share/nginx/html/proxy.pac
 
-# Squid Proxy Config File
+# Squid Proxy and Guard Config File
 sudo cp -f ./piproxy/proxy_config/squid.conf /etc/squid/squid.conf
+sudo cp -f ./piproxy/proxy_config/squidguard.conf /etc/squidguard/squidguard.conf
+
+# Update squidguard post updates
+sudo squidGuard -d -b -P -C all
+sudo chown -R proxy:proxy /var/lib/squidguard/db/
+
 
 # nginx Config File
 
