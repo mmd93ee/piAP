742452b93df8a66988cd39e903e9928777360505
Matthew Dutton <dutton.matt@gmail.com>
2019-02-09 23:40:45 +0000

    Setup Lightsquid

--- proxy_config/update_proxy_services.sh
+++ proxy_config/update_proxy_services.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 cd ~
 
-# Proxy .pac file and block page
+# Proxy .pac file, mime.types and block page
 echo "Updating pac, mime.types and block files..."
 sudo cp -f ./piproxy/proxy_config/mime.types /etc/nginx/
 sudo cp -f ./piproxy/proxy_config/proxy.pac /var/www/html/proxy.pac
 sudo cp -f ./piproxy/proxy_config/block.html /var/www/html/block.html
-
-sudo chmod 0755 /var/www/html/proxy.pac
-sudo chmod 0755 /var/www/html/block.html
 
 # Squid Proxy Config File
 sudo cp -f ./piproxy/proxy_config/squid.conf /etc/squid/squid.conf
@@ -25,6 +22,14 @@ else
 fi
 
 # nginx Config File
-echo "Updating nginx config file..."
+echo "Updating nginx config file amd adding lightsquid..."
 sudo cp -f ./piproxy/proxy_config/nginx.conf /etc/nginx/
+sudo cp -f ./piproy/proy_config/lightsquid /etc/nginx/sites-enabled/
+sudo cp -f ./piproy/proy_config/lightsquid.cfg /etc/lightsquid/lightsquid.cfg
 
+# LightSquid installation
+
+
+# Fix file permissions
+sudo chmod 0755 /var/www/html/proxy.pac
+sudo chmod 0755 /var/www/html/block.html
