ea6f5828559499e3169f8c8cc025ebc0094acdfa
Matthew Dutton <dutton.matt@gmail.com>
2019-02-09 00:18:49 +0000

    Updated squidguard

--- proxy_config/update_proxy_services.sh
+++ proxy_config/update_proxy_services.sh
@@ -4,8 +4,10 @@ cd ~
 # Proxy .pac file
 sudo cp -f ./piproxy/proxy_config/proxy.pac /usr/share/nginx/html/proxy.pac
 sudo cp -f ./piproxy/proxy_config/proxy.pac /var/www/html/proxy.pac
+sudo cp -f ./piproxy/proxy_config/block.html /var/www/html/block.html
 sudo chmod 0755 /usr/share/nginx/html/proxy.pac
 sudo chmod 0755 /var/www/html/proxy.pac
+sudo chmod 0755 /var/www/html/block.html
 
 # Squid Proxy Config File
 sudo cp -f ./piproxy/proxy_config/squid.conf /etc/squid/squid.conf
